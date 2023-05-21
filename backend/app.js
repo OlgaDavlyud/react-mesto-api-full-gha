419f02dd80ac2f01681e4855a4569b2cf23392a8
Olga Davlyud <davlyudoa@yandex.ru>
2023-05-21 22:58:17 +0300

    изменяет адрес подключения к базе данных

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,7 @@ const InternalServerError = require('./middlewares/error');
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 const app = express();
 
