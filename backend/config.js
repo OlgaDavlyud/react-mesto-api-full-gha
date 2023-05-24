2999dc31bb37465d40dfb3d3aa934717f0fe87b8
Olga Davlyud <davlyudoa@yandex.ru>
2023-05-24 23:19:33 +0300

    Начинаем заново

--- backend/config.js
+++
@@ -1,5 +0,0 @@
-const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
-
-module.exports = {
-  DB_ADDRESS
-};
--- backend/errors/GoodRequest.js
+++
@@ -1,9 +0,0 @@
-const http2 = require('node:http2');
-
-const RES_OK = http2.constants.HTTP_STATUS_OK;
-const CREATED = http2.constants.HTTP_STATUS_CREATED;
-
-module.exports = {
-  RES_OK,
-  CREATED,
-};
