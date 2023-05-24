fa5d05cdba51211e4e9576dfa05853c42ffe8b76
Olga Davlyud <davlyudoa@yandex.ru>
2023-05-24 19:33:46 +0300

    Добавляет пакет cors

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
 const { errors } = require('celebrate');
+const cors = require('cors');
 const {
   validateLogin,
   validateUser,
@@ -19,9 +20,9 @@ const { DB_ADDRESS } = require('./config');
 
 const { PORT = 3000 } = process.env;
 
-mongoose.connect(DB_ADDRESS);
-
 const app = express();
+app.use(cors());
+mongoose.connect(DB_ADDRESS);
 
 app.use(cookieParser());
 app.use(express.json());
@@ -34,6 +35,7 @@ app.use(routes);
 app.use(errors());
 app.use(InternalServerError);
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+app.listen(PORT, (err) => {
+  if (err) console.error('Unable to connect the server: ', err);
+  console.log(`🌍 Server listening on port ${PORT}`);
 });
